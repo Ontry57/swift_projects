052112264e59c2c7d48925637c9484de9f214a4b
Andrei Korolev <Andrei.Korolev@me.com>
2020-07-22 01:28:28 +0300

    lesson-8

---
+++ lesson8.iosSnake/lesson8.iosSnakeTests/lesson8_iosSnakeTests.swift
@@ -0,0 +1,34 @@
+//
+//  lesson8_iosSnakeTests.swift
+//  lesson8.iosSnakeTests
+//
+//  Created by korolev-ap on 22.07.2020.
+//  Copyright © 2020 Ontry. All rights reserved.
+//
+
+import XCTest
+@testable import lesson8_iosSnake
+
+class lesson8_iosSnakeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
